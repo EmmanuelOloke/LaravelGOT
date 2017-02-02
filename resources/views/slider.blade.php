67e831e3089d9e4a8a3b472d616f81d287cbc02a
RCCGR5JRCHURCH <RCCGR5JRCHURCH>
2017-02-02 11:43:54 +0100

    Fixed carousel image links

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+<div class="container" style="margin-top: 20px; color: salmon;">
+
 <div id="mycarousel" class="carousel slide" data-ride="carousel">
 	<!-- Indicators -->
 	<ol class="carousel-indicators">
@@ -16,49 +18,49 @@
 
 	<div class="carousel-inner" role="listbox">
 		<div class="item">
-			<img src="/img/shiffi0.jpg" data-color="lightblue" alt="First Image">
+			<img src="{{asset('assets/img/shiffi0.jpg')}}" data-color="lightblue" alt="First Image">
 			<div class="carousel-caption">
 				<h3>First Image</h3>
 			</div>
 		</div>
 
 		<div class="item">
-			<img src="/img/shiffi1.jpg" data-color="firebrick" alt="Second Image">
+			<img src="{{asset('assets/img/shiffi1.jpg')}}" data-color="firebrick" alt="Second Image">
 			<div class="carousel-caption">
 				<h3>Second Image</h3>
 			</div>
 		</div>
 
 		<div class="item">
-			<img src="/img/shiffi2.jpg" data-color="violet" alt="Third Image">
+			<img src="{{asset('assets/img/shiffi2.jpg')}}" data-color="violet" alt="Third Image">
 			<div class="carousel-caption">
 				<h3>Third Image</h3>
 			</div>
 		</div>
 
 		<div class="item">
-			<img src="/img/shiffi3.jpg" data-color="lightgreen" alt="Fourth Image">
+			<img src="{{asset('assets/img/shiffi3.jpg')}}" data-color="lightgreen" alt="Fourth Image">
 			<div class="carousel-caption">
 				<h3>Fourth Image</h3>
 			</div>
 		</div>
 
 		<div class="item">
-			<img src="/img/shiffi4.jpg" data-color="tomato" alt="Fifth Image">
+			<img src="{{asset('assets/img/shiffi4.jpg')}}.jpg" data-color="tomato" alt="Fifth Image">
 			<div class="carousel-caption">
 				<h3>Fifth Image</h3>
 			</div>
 		</div>
 
 		<div class="item">
-			<img src="/img/shiffi5.jpg" data-color="salmon" alt="Sixth Image">
+			<img src="{{asset('assets/img/shiffi5.jpg')}}" data-color="salmon" alt="Sixth Image">
 			<div class="carousel-caption">
 				<h3>Sixth God!</h3>
 			</div>
 		</div>
 	</div>
 
-	<!-- Controls -->
+	 <!-- Controls  -->
 	<a class="left carousel-control" href="#mycarousel" role="button" data-slide="prev">
 		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 		<span class="sr-only">Previous</span>
@@ -69,5 +71,5 @@
 		<span class="sr-only">Next</span>
 	</a>
 </div>
-
+</div>
 @endsection
